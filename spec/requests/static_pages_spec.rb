26baa3f02042e48315cee58332b6c23df7cc5a9b
felipefujioka <felipefujioka@gmail.com>
2013-02-03 20:06:52 +0100

    style problem

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,26 +1,24 @@
 require 'spec_helper'
+require 'application_helper'
 
 describe "Static Pages" do
+  
   describe "Home page" do
-    it "should have the content 'First app'" do
-      visit '/static_pages/home'
+    it "should have the content 'Haggendazz'" do
+      visit root_path
       page.should have_content('Haggendazz')
     end
     
     it "should have the base title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => 'Haggendazz')
     end
     
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => '| Home')
-    end
   end
   
   describe "Help Page" do
     it "should have the content 'HELP'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
     
@@ -28,8 +26,21 @@ describe "Static Pages" do
   
   describe "About Us" do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About Us')
     end
+  end
+  
+  describe "Contact page" do
+    it "should have the h1 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', :text => 'Contact')
+    end
+    
+    it "should have the title 'Contact'" do
+      visit contact_path
+      page.should have_selector('title', :text => "Haggendazz | Contact")
+    end
+    
   end
 end
